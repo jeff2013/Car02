4329709b4a23b7dc8138416a7051c44383595f62
reedmullanix <reedmullanix@gmail.com>
2015-04-26 18:30:07 -0700

    Added some documentation

--- app/src/main/java/com/example/jeff/car02/DataSource/DataTypes.java
+++ app/src/main/java/com/example/jeff/car02/DataSource/DataTypes.java
@@ -1,7 +1,8 @@
 package com.example.jeff.car02.DataSource;
 
 /**
- * Created by reed on 3/27/15.
+ * Author: Reed Mullanix
+ * An Enum of all data types that we are going to graph
  */
 public enum DataTypes {
     TIME, DISTANCE, SPEED, FUEL_EFFICIENCY, DELTA_FUEL, TOTAL_FUEL, TOTAL_CO2
