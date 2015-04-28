4f61ccf8b6b89507545cef8ac3657c957c5d1a42
Finn Hackett <finn.hackett@maintool.me>
2015-04-28 15:29:14 -0700

    added some logging

--- app/src/main/java/com/example/jeff/car02/fragments/Fragment_section3.java
+++ app/src/main/java/com/example/jeff/car02/fragments/Fragment_section3.java
@@ -45,50 +45,63 @@ public class Fragment_section3 extends SupportMapFragment {
         dataSource.addObserver(new Observer() {
             @Override
             public void update(Observable observable, Object data) {
-                List<LatLng> positions = dataSource.getLocations();
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        List<LatLng> positions = dataSource.getLocations();
 
-                float maxData = dataSource.getMaxY();
+                        float maxData = dataSource.getMaxY();
 
-                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
 
-                LatLng last_pos = null;
-                if(positions.size()>0){
-                    last_pos = positions.get(0);
-                    builder.include(positions.get(0));
-                }
-                List<LatLng> points = new ArrayList<LatLng>(2);
-                Log.d("Map update", "called, num_points = " + Integer.toString(positions.size()));
+                        LatLng last_pos = null;
+                        if (positions.size() > 0)
 
-                int i=1;
-                for (; i < dataSource.size() && i < positions.size(); ++i) {
-                    float normalisedData = dataSource.getY(i).floatValue() / maxData;
-                    float colour = (BitmapDescriptorFactory.HUE_RED - BitmapDescriptorFactory.HUE_BLUE) * normalisedData + BitmapDescriptorFactory.HUE_BLUE;
-                    colour = colour > 359 ? 359 : colour;
-                    colour = colour < 0 ? 0 : colour;
+                        {
+                            last_pos = positions.get(0);
+                            builder.include(positions.get(0));
+                        }
 
-                    builder.include(positions.get(i));
+                        List<LatLng> points = new ArrayList<LatLng>(2);
+                        Log.d("Map update", "called, num_points = " + Integer.toString(positions.size()));
 
-                    if(i<segments.size()){
-                        Polyline line = segments.get(i);
-                        line.setVisible(true);
-                        points.set(0, last_pos);
-                        points.set(1,positions.get(i));
-                        line.setPoints(points);
-                        line.setColor((int)colour);
-                    }else{
-                        PolylineOptions opts = new PolylineOptions();
-                        opts.color((int)colour);
-                        opts.add(last_pos,positions.get(i));
-                        opts.visible(true);
-                        segments.add(map.addPolyline(opts));
+                        int i = 1;
+                        for (; i < dataSource.size() && i < positions.size(); ++i)
+
+                        {
+                            float normalisedData = dataSource.getY(i).floatValue() / maxData;
+                            float colour = (BitmapDescriptorFactory.HUE_RED - BitmapDescriptorFactory.HUE_BLUE) * normalisedData + BitmapDescriptorFactory.HUE_BLUE;
+                            colour = colour > 359 ? 359 : colour;
+                            colour = colour < 0 ? 0 : colour;
+
+                            builder.include(positions.get(i));
+
+                            if (i < segments.size()) {
+                                Polyline line = segments.get(i);
+                                line.setVisible(true);
+                                points.set(0, last_pos);
+                                points.set(1, positions.get(i));
+                                line.setPoints(points);
+                                line.setColor((int) colour);
+                            } else {
+                                PolylineOptions opts = new PolylineOptions();
+                                opts.color((int) colour);
+                                opts.add(last_pos, positions.get(i));
+                                opts.visible(true);
+                                segments.add(map.addPolyline(opts));
+                            }
+                            last_pos = positions.get(i);
+                        }
+
+                        for (; i < segments.size(); ++i)
+
+                        {
+                            segments.get(i).setVisible(false);
+                        }
+
+                        map.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 100));
                     }
-                    last_pos = positions.get(i);
-                }
-                for(;i<segments.size();++i) {
-                    segments.get(i).setVisible(false);
-                }
-
-                map.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 100));
+                });
             }
         });
         Thread dataThread = new Thread(dataSource);
